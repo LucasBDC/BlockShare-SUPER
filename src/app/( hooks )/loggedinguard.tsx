3b9d42a6f0d66eb49d3524d4b41d2571152b2bb0
LucasBDC <jlucas.bettiol@gmail.com>
2023-09-27 22:30:14 -0400

    trynna fix the bug

--- src/app/( hooks )/loggedinguard.tsx
+++ src/app/( hooks )/loggedinguard.tsx
@@ -1,19 +1,30 @@
 // Import dependencies
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { useRouter } from "next/navigation";
 import { auth } from "@/app/( firebase )/firebase";
+import Login from "../( pages )/( auth )/login/page";
 
 // Function component
 export default function LoggedInGuard({ children }: { children: React.ReactNode }): React.ReactElement {
   // Router
   const router = useRouter();
+  const isLoggedIn = useRef(false);
 
   // Check if the user is logged in
-  if(!auth.currentUser){
-    router.push('/login')
+  useEffect(() => {
+    isLoggedIn.current = auth.currentUser !== null;
+  }, [auth.currentUser]);
+
+  // Conditionally render the dashboard
+  if (isLoggedIn.current) {
+    return (
+      <div className="flex h-screen justify-center items-center">
+        <h1>Welcome to the dashboard, <span className="font-semibold">{auth.currentUser?.displayName}</span></h1>
+      </div>
+    );
+  } else {
+    // Redirect the user to the login page
+    router.push("/login");
+    return <Login/>;
   }
-    return(
-      <div>{children}</div>
-    )
-  
 }
