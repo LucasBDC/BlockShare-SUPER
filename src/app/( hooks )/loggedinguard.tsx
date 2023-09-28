0ba3e2c55dae49a0fbcb773fe892487efc8322a9
Jose Lucas Costa <joselucascosta@MacBook-Air-de-Jose.local>
2023-09-27 21:34:58 -0400

    oi

--- src/app/( hooks )/loggedinguard.tsx
+++ src/app/( hooks )/loggedinguard.tsx
@@ -1,22 +1,19 @@
-'use client'
 // Import dependencies
 import React, { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { auth } from '@/app/( firebase )/firebase';
 
 // Function component
-const LoggedInGuard = ({ children }: { children: React.ReactNode }): React.ReactElement => {
+export default function LoggedInGuard({ children }: { children: React.ReactNode }): React.ReactElement {
   // Router
   const router = useRouter();
 
   // Check if the user is logged in
-  if (!auth.currentUser) {
-    router.push('/');
+  if(!auth.currentUser){
+    router.push('/login')
   }
 
-  return (
-    <div>{children}</div>
-  );
-};
-
-export default LoggedInGuard;
+  return(
+    <div>children</div>
+  )
+}
