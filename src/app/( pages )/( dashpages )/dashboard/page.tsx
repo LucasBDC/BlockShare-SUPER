d97b4d5e090affd9eb5ed2dcfb65fbd8d9e360d8
LucasBDC <jlucas.bettiol@gmail.com>
2023-09-28 00:51:14 -0400

    Trying hard to fix the loading dashboard bug

--- src/app/( pages )/( dashpages )/dashboard/page.tsx
+++ src/app/( pages )/( dashpages )/dashboard/page.tsx
@@ -1,15 +1,12 @@
 'use client'
-import LoggedInGuard from "@/app/( hooks )/loggedinguard"
+// import LoggedInGuard from "@/app/( hooks )/loggedinguard"
 import { getAuth } from "firebase/auth"
 import {auth} from '@/app/( firebase )/firebase'
-import { useRouter } from "next/navigation"
-import { useEffect } from "react"
 
 export default function Dashboard(){
     return(
         <div className="flex h-screen justify-center items-center">
                 <h1 className="text-center">Welcome to the dashboard, <span className="font-semibold">{auth.currentUser?.displayName}</span></h1>
-
         </div>
     )
 }
