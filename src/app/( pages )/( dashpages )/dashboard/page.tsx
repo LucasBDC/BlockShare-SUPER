702d44a24fe5a9a3d0cf4bdf48c27082afb95284
LucasBDC <jlucas.bettiol@gmail.com>
2023-09-27 22:45:26 -0400

    trynna fix the bug

--- src/app/( pages )/( dashpages )/dashboard/page.tsx
+++ src/app/( pages )/( dashpages )/dashboard/page.tsx
@@ -6,9 +6,8 @@ import {auth} from '@/app/( firebase )/firebase'
 export default function Dashboard(){
     return(
         <div className="flex h-screen justify-center items-center">
-            <LoggedInGuard>
+            
                 <h1 className="text-center">Welcome to the dashboard, <span className="font-semibold">{auth.currentUser?.displayName}</span></h1>
-            </LoggedInGuard>
         </div>
     )
 }
