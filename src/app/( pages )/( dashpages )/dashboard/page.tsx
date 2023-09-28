127457f637dea2204383904e45f4ae56a0da17ea
LucasBDC <jlucas.bettiol@gmail.com>
2023-09-28 00:47:12 -0400

    Trying hard to fix the loading dashboard bug

--- src/app/( pages )/( dashpages )/dashboard/page.tsx
+++ src/app/( pages )/( dashpages )/dashboard/page.tsx
@@ -8,9 +8,8 @@ import { useEffect } from "react"
 export default function Dashboard(){
     return(
         <div className="flex h-screen justify-center items-center">
-            <LoggedInGuard>
                 <h1 className="text-center">Welcome to the dashboard, <span className="font-semibold">{auth.currentUser?.displayName}</span></h1>
-            </LoggedInGuard>
+
         </div>
     )
 }
