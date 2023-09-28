39cb23fbbd121b63363d7ca2e93280feddacbab5
LucasBDC <jlucas.bettiol@gmail.com>
2023-09-27 22:16:57 -0400

    trynna fix the bug

--- src/app/( hooks )/loggedinguard.tsx
+++ src/app/( hooks )/loggedinguard.tsx
@@ -10,6 +10,7 @@ export default function LoggedInGuard({ children }: { children: React.ReactNode 
 
   // Check if the user is logged in
   if(!auth.currentUser){
+    return(<div></div>)
     router.push('/login')
   }
   else{
