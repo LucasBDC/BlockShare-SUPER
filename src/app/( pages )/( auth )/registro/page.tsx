f9dd6c69f9e3649ccacc731dc1ea2659b27c4b04
LucasBDC <jlucas.bettiol@gmail.com>
2024-02-07 23:59:01 -0400

    asdads

--- src/app/( pages )/( auth )/registro/page.tsx
+++ src/app/( pages )/( auth )/registro/page.tsx
@@ -1,15 +1,21 @@
-'use client'
-import { getAuth, createUserWithEmailAndPassword, updateProfile, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+"use client";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  updateProfile,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
 import { SetStateAction, useState } from "react";
-import {} from '@/app/( firebase )/firebase'
+import {} from "@/app/( firebase )/firebase";
 import { useRouter } from "next/navigation";
-import {auth, app} from '@/app/( firebase )/firebase'
+import { auth, app } from "@/app/( firebase )/firebase";
 import Link from "next/link";
 import Image from "next/image";
 
 import { getFirestore, doc, setDoc } from "firebase/firestore";
 
-import Logo from '@/assets/logo.svg'
+import Logo from "@/assets/logo.svg";
 
 // Components
 import StyledInputs from "@/components/styledinputs";
@@ -29,64 +35,62 @@ interface User {
 
 // Function component
 export default function Registro() {
-  const router =  useRouter()
+  const router = useRouter();
   // State variables
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [name, setName] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
   const [age, setAge] = useState(0);
-  const [gender, setGender] = useState('');
+  const [gender, setGender] = useState("");
 
   async function handleSignUp() {
-    if(typeof window !== 'undefined') {
-      // Create a new user with Firebase Auth
+    if (typeof window !== "undefined") {
       const userCredential = await createUserWithEmailAndPassword(
         auth,
         email,
         password
       );
-  
-      // Get the authenticated user
       const user = userCredential.user;
-  
-      // Update the user's profile
-       updateProfile(user, {
+      updateProfile(user, {
         displayName: name,
       });
-  
-      // Save the user's age and gender to Firestore
-  
-      // Clear the sign up form
-      setEmail('');
-      setPassword('');
-      setName('');
+      setEmail("");
+      setPassword("");
+      setName("");
       setAge(0);
-      setGender('');
-  
-      // Display an alert message to the user
-      alert('Sign up successful!');
-      router.push('/login')
+      setGender("");
+      alert("Sign up successful!");
+      router.push("/login");
     } else {
-      // Display an error message to the user
       alert(`Sign up failed`);
     }
   }
 
-  
   return (
     <Printela>
-        <form onSubmit={handleSignUp} method="post" className="border-4 border-slate-600 px-16 py-10 rounded-tl-3xl rounded-br-3xl gap-2 flex flex-col max-2xl:w-[35%] max-sm:w-[100%] max-lg:w-[70%]">
-          <Link href='/' className="flex items-center justify-center gap-4 mb-5">
-            <Image src={Logo} alt="/" className="w-14"/>
-            <p className="uppercase font-semibold text-2xl animate-pulse">blockshare</p>
-          </Link>
-          <hr style={{borderColor: 
-          'linear-gradient(270deg, #585858 0%, rgba(88, 88, 88, 0.00) 100%)'}} className="border border-slate-600 mb-3"/>
+      <form
+        onSubmit={handleSignUp}
+        method="post"
+        className="border-4 border-slate-600 px-16 py-10 rounded-tl-3xl rounded-br-3xl gap-2 flex flex-col max-2xl:w-[35%] max-sm:w-[100%] max-lg:w-[70%]"
+      >
+        <Link href="/" className="flex items-center justify-center gap-4 mb-5">
+          <Image src={Logo} alt="/" className="w-14" />
+          <p className="uppercase font-semibold text-2xl animate-pulse">
+            blockshare
+          </p>
+        </Link>
+        <hr
+          style={{
+            borderColor:
+              "linear-gradient(270deg, #585858 0%, rgba(88, 88, 88, 0.00) 100%)",
+          }}
+          className="border border-slate-600 mb-3"
+        />
         <StyledInputs
           type="email"
           placeholder="E-Mail"
           value={email}
-          onchange={(e: { target: { value: SetStateAction<string>; }; }) => {
+          onchange={(e: { target: { value: SetStateAction<string> } }) => {
             // You need to cast the event target value to a string here
             setEmail(e.target.value as string);
           }}
@@ -95,7 +99,7 @@ export default function Registro() {
           type="password"
           placeholder="Senha"
           value={password}
-          onchange={(e: { target: { value: SetStateAction<string>; }; }) => {
+          onchange={(e: { target: { value: SetStateAction<string> } }) => {
             // You need to cast the event target value to a string here
             setPassword(e.target.value as string);
           }}
@@ -104,7 +108,7 @@ export default function Registro() {
           type="text"
           placeholder="Nome"
           value={name}
-          onchange={(e: { target: { value: SetStateAction<string>; }; }) => {
+          onchange={(e: { target: { value: SetStateAction<string> } }) => {
             // You need to cast the event target value to a string here
             setName(e.target.value as string);
           }}
@@ -113,19 +117,37 @@ export default function Registro() {
           type="number"
           placeholder="Idade"
           value={age}
-          onchange={(e: { target: { value: SetStateAction<number>; }; }) => {
+          onchange={(e: { target: { value: SetStateAction<number> } }) => {
             // You need to cast the event target value to a number here
             setAge(e.target.value as number);
           }}
         />
-        <select value={gender} onChange={(e) => setGender(e.target.value)} className="text-slate-400 border-slate-500 border-2  w-[100%] py-3 px-2 rounded-bl-md rounded-br-md" style={{background: 'linear-gradient(0deg, rgba(88, 88, 88, 0.15) 0%, rgba(88, 88, 88, 0.15) 100%), rgba(208, 208, 208, 0.05)'}}>
+        <select
+          value={gender}
+          onChange={(e) => setGender(e.target.value)}
+          className="text-slate-400 border-slate-500 border-2  w-[100%] py-3 px-2 rounded-bl-md rounded-br-md"
+          style={{
+            background:
+              "linear-gradient(0deg, rgba(88, 88, 88, 0.15) 0%, rgba(88, 88, 88, 0.15) 100%), rgba(208, 208, 208, 0.05)",
+          }}
+        >
           <option value="">Selecione o gênero</option>
           <option value="male">Masculino</option>
           <option value="female">Feminino</option>
         </select>
-        <StyledButtons texto="Cadastrar"/>
-        <Link href='/login' className="flex gap-2 self-center group items-center"><p className="text-slate-400 text-sm group-hover:text-slate-200">Já tem uma conta?</p><span className="font-semibold text-purple-600 text- group-hover:text-purple-500 group-active:text-purple-800">Entre</span></Link>
-        </form>
+        <StyledButtons texto="Cadastrar" />
+        <Link
+          href="/login"
+          className="flex gap-2 self-center group items-center"
+        >
+          <p className="text-slate-400 text-sm group-hover:text-slate-200">
+            Já tem uma conta?
+          </p>
+          <span className="font-semibold text-purple-600 text- group-hover:text-purple-500 group-active:text-purple-800">
+            Entre
+          </span>
+        </Link>
+      </form>
     </Printela>
   );
 }
